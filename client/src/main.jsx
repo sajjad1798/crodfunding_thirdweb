1324d74a2d86e3fa87719a7a65ced3769892c64d
sajjad <muhammadsajjad1798@gmail.com>
2022-12-19 11:31:50 +0500

    add client side project and setup tailwindcss

---
+++ client/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import "./styles/globals.css";
+
+// This is the chainId your dApp will work on.
+const activeChainId = ChainId.Goerli;
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(
+  <React.StrictMode>
+    <ThirdwebProvider desiredChainId={activeChainId}>
+      <App />
+      <Router />
+    </ThirdwebProvider>
+  </React.StrictMode>
+);
